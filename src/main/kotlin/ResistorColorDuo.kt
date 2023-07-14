8b0c79bd90fbf9e6fa8d582a23d801d4c466b632
Panos Matsinopoulos <panagiotis@matsinopoulos.gr>
2023-07-14 12:09:17 +0300

    Simpler solution with string concatenation and .toInt()

--- src/main/kotlin/ResistorColorDuo.kt
+++ src/main/kotlin/ResistorColorDuo.kt
@@ -1,7 +1,3 @@
-import kotlin.math.pow
-
 object ResistorColorDuo {
-    fun value(vararg colors: Color) = colors.take(2).reversed().foldIndexed(0) { index, acc, color ->
-        acc + color.ordinal * 10.toDouble().pow(index.toDouble()).toInt()
-    }
+    fun value(vararg colors: Color) = "${colors[0].ordinal}${colors[1].ordinal}".toInt()
 }
